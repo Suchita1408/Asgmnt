2b3b97fbb20d3f2473685ef6f1d6021dd96d1ce0
Suchita1408 <39963487+Suchita1408@users.noreply.github.com>
2018-09-08 06:35:28 +0530

    Updated changes

--- TestProject/src/com/test/MyBrowserClass.java
+++ TestProject/src/com/test/MyBrowserClass.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.csv.CSVFormat;
@@ -15,45 +16,23 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class MyBrowserClass 
+public class MyBrowserClass extends BaseClass
 {
 	WebDriver driver;
-	String driverPath;
-	String baseUrl;
-	String email;
-	String pass ;
+	Map<String, String> csvData;
 	@BeforeTest
-	public void setup() throws IOException
+	public void setup() 
 	{
-		Reader reader = Files.newBufferedReader(Paths.get("Resource/Data.csv"));
-		 @SuppressWarnings("resource")
-		CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
-				  .withFirstRecordAsHeader()
-                .withIgnoreHeaderCase()
-                .withTrim());
-		 for (CSVRecord csvRecord : csvParser) {
-             // Accessing values by the names assigned to each column
-			 driverPath = csvRecord.get("driverPath");
-			 baseUrl  = csvRecord.get("baseUrl");
-			 email= csvRecord.get("email");
-			 pass= csvRecord.get("pass");
-		 }
-		System.setProperty("webdriver.chrome.driver", driverPath);
-		driver= new ChromeDriver();
-		
-		driver.get(baseUrl);
-		
-		driver.manage().window().maximize();
-		
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		csvData=getDataFromCSV();
+		driver=getWebDriver(csvData.get("browser"),csvData.get("baseUrl"));		
 	}
 	
 	@Test
 	public void signUpFunction()
 	{
-
+				
 		SaleLoginPage log= new SaleLoginPage(driver);
-		log.loginMethod( email,  pass);
+		log.loginMethod( csvData.get("email"),  csvData.get("pass"));
 		
 	}
 	
